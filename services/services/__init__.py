3e85e7f0d9f5895db210832d3f9af78eb1544c3d
abdelmonem awad <eg2@live.com>
2025-07-05 14:44:56 +0400

    eg.
    eg.

--- services/services/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Services package for AI Translator
-حزمة الخدمات للترجمان الآلي
-"""
-
-from .remote_storage import RemoteStorageManager, setup_remote_mount, get_mount_status
-
-__all__ = ['RemoteStorageManager', 'setup_remote_mount', 'get_mount_status']
